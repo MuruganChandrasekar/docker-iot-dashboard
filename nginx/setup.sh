1984195393e6dc39da3c8c52606bb3cd5f6fdde0
Murugan Chandrasekar <cmurugan@dashboard-1.mcci.io>
2021-03-03 10:42:24 +0000

    MONGODB SSL Termination

--- nginx/setup.sh
+++ nginx/setup.sh
@@ -58,7 +58,8 @@ if [ "$CERTBOT_TEST" != "test" ]; then
     /usr/sbin/nginx -s stop && echo "stopped successfully"
 fi
 
-sed -i $'/http {/{e cat /root/mongo.txt\n}' /etc/nginx/nginx.conf
+# Configuring Mongodb connection access using Nginx SSL Termination method
+sed -i "s/domain/$CERTBOT_DOMAINS/g" /root/mongo.txt && sed -i $'/http {/{e cat /root/mongo.txt\n}' /etc/nginx/nginx.conf
 
 # now, add the fields to the virtual host section for https.
 set -- proxy-*.conf
